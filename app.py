ff1fa2d9461260f06c59d8a71007a132ce142074
Shreel17 <shreelgandhi1709@gmail.com>
2026-02-02 00:11:20 +0530

    Modify sys.path to include frontend directory
    Add sys.path modification to include frontend directory

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
+import sys
+import os
+
+# Isse frontend folder ke andar ki files ek dusre ko pehchan lengi
+sys.path.append(os.path.join(os.path.dirname(__file__), 'frontend'))
+
 from frontend.app import *
+
